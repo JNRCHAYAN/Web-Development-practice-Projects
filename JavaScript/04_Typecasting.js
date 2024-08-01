c541502206a1e8e188d752bdf8b1113583b175c9
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-01 16:46:43 +0600

    04_Typecasting

---
+++ JavaScript/04_Typecasting.js
@@ -0,0 +1,10 @@
+var n1 ="19";
+var n2 = "33";
+
+console.log(n1+n2);
+
+// Now convert this string to int value
+
+n1 = parseInt(n1);
+n2 = parseInt(n2);   // Also we can convert others type like parseFloat
+console.log(n1+n2);
