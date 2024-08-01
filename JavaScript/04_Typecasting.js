b82271400d4cfb45bf542629a425a38910ae3d17
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-01 16:53:33 +0600

    Typecasting

--- JavaScript/04_Typecasting.js
+++ JavaScript/04_Typecasting.js
@@ -2,9 +2,13 @@ var n1 ="19";
 var n2 = "33";
 
 console.log(n1+n2);
+console.log(typeof n1);
 
-// Now convert this string to int value
+// Now convert this string to Number value
 
 n1 = parseInt(n1);
 n2 = parseInt(n2);   // Also we can convert others type like parseFloat
 console.log(n1+n2);
+console.log(typeof n1);
+
+// n1= n1.toFixed(2)  Show How to many number show after float 
