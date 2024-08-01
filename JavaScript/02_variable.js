49da76220987e483e3c11f3153e5015f73254c9b
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-01 16:31:29 +0600

    Varible

--- JavaScript/02_variable.js
+++ JavaScript/02_variable.js
@@ -1,8 +1,11 @@
 var myname = "chayan";
 var n1 =10;
-var n2 =20;
+var n2 =20;  //Numeric data type
 var avg = (n1+n2)/2
 
+var traust = true; // Bolean Data type
+
 console.log(myname);
-console.log("Average value of two number :  ".avg);
+console.log(traust);
+console.log("Average value of two number :  " + avg);
 
