5326631c82c5aaddfd6e25da9f46871446ee010a
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-01 16:34:38 +0600

    Operation

--- JavaScript/03_Operation.js
+++ JavaScript/03_Operation.js
@@ -3,5 +3,8 @@ var n2 =20;
 var avg = (n1+n2)/2;
 
 console.log("Average value of two number :  " + avg);
-console.log("N1+N2"+(n1+n2));
+console.log("N1+N2 : "+(n1+n2));
+console.log("N1-N2 : "+(n1-n2));
+console.log("N1*N2 : "+(n1*n2));
+console.log("N1/N2 : "+(n1/n2));
 
