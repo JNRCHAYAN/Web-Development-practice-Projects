9072a669c2b3ee85c3e6b0ae273d38883f6ddfab
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-04 00:46:36 +0600

    Array

--- JavaScript/05_Array.js
+++ JavaScript/05_Array.js
@@ -1,4 +1,22 @@
-var arr = [10, 3, 3, 5, 3, 6, 3 ];
+var arr = [10, 3, 3, 5, 3, 6, 3,"Koy" ];
 var arrname = ["Chayan", "Roni" ,"Miltan","Miraz"];
 
 console.log(arr);
+console.log(arrname);
+console.log(arrname.length);
+console.log(arr[3]);  //Index value print
+arr[3] = "Das";
+console.log(arr[3]); 
+console.log(arr);
+
+// Find index of an element 
+
+var possition = arr.indexOf("Das");
+console.log(possition);
+
+// Push and pop funtion apply in array 
+var number = [19,4,45,3,5,3];
+number.push("Chayan");
+console.log(number);
+number.pop();  // Last Element Remove 
+console.log(number);
