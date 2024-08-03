e56cff696305951d50e2e1bb72ed8334017d0011
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-04 00:59:32 +0600

    if else condition

---
+++ JavaScript/06_If_Else_Condition.js
@@ -0,0 +1,14 @@
+var num1 =20;
+var num2 =20;
+if(num1>num2)
+{
+    console.log("It is true");
+}
+else if(num1>=num2)
+{
+    console.log("Both are equal");
+}
+else
+{ 
+    console.log("It is False");
+}
