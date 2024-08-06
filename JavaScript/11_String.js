432169498b13abcb0090db2f8171b8eb73446aef
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 09:48:37 +0600

    String

---
+++ JavaScript/11_String.js
@@ -0,0 +1,13 @@
+let name0 = "Chayan Roy";
+let name1 = 'Roni Roy';
+console.log(name0)
+console.log(name1)
+console.log(name1[0])
+console.log(name0.length)
+
+//Template Literals 
+
+let n1 = "Chayan"
+let n2 = "Roy"
+let sentence = `Full name ${n1} ${n2}`
+console.log(sentence)
