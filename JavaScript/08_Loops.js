ccd40cd232de8fb01a0747a0da7af31a16771656
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 08:17:17 +0600

    For in loop

---
+++ JavaScript/08_Loops.js
@@ -0,0 +1,22 @@
+// Take user input 
+// let n = prompt("Enter The value of n");
+// n = Number.parseInt(n);
+
+// For Loop 
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+
+// For in loop 
+let obj = {
+    chayan :100,
+    shubh:50,
+    shiv:30,
+    ritika:34
+}
+
+for(let a in obj)
+{
+    console.log("Markes of "+a+ " are : "+obj[a])
+}
