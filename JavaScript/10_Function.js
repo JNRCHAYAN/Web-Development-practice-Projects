70077d5ee440350cdd439d20fae96bc460d2a965
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 08:40:52 +0600

    function

---
+++ JavaScript/10_Function.js
@@ -0,0 +1,12 @@
+function math(x ,y)
+{
+    return (x+y)/2
+}
+
+// New Formate to create a fucntion in javascript 
+const hello = ()=>
+{
+    console.log("Hello world")
+}
+console.log("Avg of two number : ",math(10,20))
+console.log(hello())
