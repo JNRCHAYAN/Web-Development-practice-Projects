2ddc0363a6970b4f10997c4ddd866682317260db
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 10:04:58 +0600

    String Method

---
+++ JavaScript/11_Method_String.js
@@ -0,0 +1,15 @@
+let name1 = "Chayan Roy"
+let name2 = "Roni Roy"
+console.log(name1.length)
+
+console.log(name1.toUpperCase())
+console.log(name1.toLowerCase())
+console.log(name1.slice(2,4))
+console.log(name1.slice(2))
+console.log(name1.replace("Chayan","Roni"))
+console.log(name1.concat(" is a friend of ",name2))
+
+let myt = "     JNR     "
+console.log(myt)
+console.log(myt.trim())
+console.log(myt.trim())
