8aae18d17f534b7be1c1956cde779e16eb5b4c1f
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-06 08:29:55 +0600

    Loop

--- JavaScript/08_Loops.js
+++ JavaScript/08_Loops.js
@@ -21,8 +21,27 @@ for(let a in obj)
     console.log("Markes of "+a+ " are : "+obj[a])
 }
 
-// for of loops 
+// for of loop
 for(let b of "Chayan")
     {
         console.log(b)
     }
+
+// While Loop 
+let i = 0;
+while(i<10)
+{
+    console.log(i)
+    i++;
+}
+
+// Do while Loop 
+
+let j = 20;
+do
+{
+    console.log(j)
+    j++;
+}while(j<25)
+
+
