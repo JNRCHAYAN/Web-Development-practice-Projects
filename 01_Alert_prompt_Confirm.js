63a25a0a598debb7f584646cecd75a8b37ba885e
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-07 12:33:23 +0600

    web dev javascript

---
+++ 01_Alert_prompt_Confirm.js
@@ -0,0 +1,28 @@
+// *** use alert prompt and confirm ***
+
+alert("Hello My Webstie")
+alert("Enter The value of a : ")
+let a = prompt("Enter a Here" ) 
+// Set Prompt defult value  
+// let a = prompt("Enter a Here" ,"23")  
+
+// Conver this string input in a number
+// a = Number.parseInt(a)
+alert("You entered a of type : "+(typeof a))
+let write= confirm("Do You want to write it on the page")
+if(write)
+{
+    document.write(a)
+}
+else
+{
+    document.write("Please allow me to write")
+}
+
+
+
+
+
+
+
+// console.table(console) // Show console all methode 
