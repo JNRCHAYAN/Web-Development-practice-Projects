fe3300f095f3c9b5d7b4369aa2b90c869e89ab9d
Jogendra nath roy chayan <119512782+JNRCHAYAN@users.noreply.github.com>
2024-08-08 15:30:15 +0600

    Dom

---
+++ Web Dev HTML CSS JAVASCRIPT'/03_Dom.js
@@ -0,0 +1,16 @@
+// console.log(document.body.firstChild)
+// console.log(document.body.lastChild)
+// console.log(document.body.childNodes)
+// console.log(document.body.hasChildNodes)
+
+// let arr = Array.from(document.body.childNodes) // Create a child array in DOM
+// console.log(arr)
+
+
+console.log(document.body.firstChild)
+a =document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
+
+
